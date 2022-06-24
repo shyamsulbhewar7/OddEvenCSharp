5a9bcc8ddeafafffcda4a929e759662b9c531237
shyamsulbhewar7 <shyamsulbhewar@gmail.com>
2022-06-24 20:31:01 +0530

    odd even number upto 100

---
+++ Model/OddEvenGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddEven.Model
+{
+    public class OddEvenGame
+    {
+        public void Odd()
+        {
+            for(int i=1;i < 100; i+= 2)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        public void Even()
+        {
+            for (int i = 2; i <= 100; i+= 2)
+            {
+                Console.Write(i + " ");
+            }
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OddEven.Model;
+using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OddEvenGame numbers = new OddEvenGame();
+            Console.WriteLine("Odd Numbers :");
+            numbers.Odd();
+            Console.WriteLine();
+            Console.WriteLine("Even Numbers :");
+            numbers.Even();
+        }
+    }
+}
